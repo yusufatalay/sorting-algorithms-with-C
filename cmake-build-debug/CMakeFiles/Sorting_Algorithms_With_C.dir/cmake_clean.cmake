86b233ffe53b415ab85da94c8d2bc4d03fd16da8
yusufatalay <yusuftahaatalay14@gmail.com>
2021-05-06 22:12:19 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Sorting_Algorithms_With_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sorting_Algorithms_With_C.dir/main.c.obj"
+  "Sorting_Algorithms_With_C.exe"
+  "Sorting_Algorithms_With_C.exe.manifest"
+  "Sorting_Algorithms_With_C.lib"
+  "Sorting_Algorithms_With_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sorting_Algorithms_With_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Sorting_Algorithms_With_C.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sorting_Algorithms_With_C.
+# This may be replaced when dependencies are built.
